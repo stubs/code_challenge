877c0dd8e35a01682356879ca19f70d35bea863d
Aaron D. Gonzalez <a.gonzalez979@gmail.com>
2016-07-20 22:21:09 -0400

    Update __init__.py
    Remove commented out code.

--- famous_people/famous_people/__init__.py
+++ famous_people/famous_people/__init__.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
-from flask import Flask #, session, render_template, redirect, url_for
-#from flask_bootstrap import Bootstrap
-#from flask_sqlalchemy import SQLAlchemy
-#from flask.ext.wtf import Form
-#from wtforms import RadioField, SubmitField
+from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
-#app.config.from_object('config')
 app.config.from_pyfile('config.py')
-#bootstrap = Bootstrap(app)
-#db = SQLAlchemy(app)
+
 
 import famous_people.views
